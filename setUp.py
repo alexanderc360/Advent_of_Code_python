4c4d46a1e9eff86dcda733a7106b2e2fec2ad41f
Alex Cohen <alexander.c.3600@gmail.com>
2025-12-02 20:25:56 -0500

    improved path creation in the setup script

--- setUp.py
+++ setUp.py
@@ -1,6 +1,6 @@
 import datetime
 import sys
-import os
+from pathlib import Path
 
 args = sys.argv
 year, day = 0, 0
@@ -12,8 +12,11 @@ elif len(args) == 2:
     if 1 <= int(args[1]) <= 25:
         day = int(args[1])
         year = datetime.datetime.today().year
+        if datetime.datetime.today().month < 12:
+            year -= 1
+            print(f"Advent hasn't started yet! Defaulting to {year}")
 elif len(args) == 3:
-    if 2015 <= int(args[1]) <= 2024:
+    if 2015 <= int(args[1]) <= datetime.datetime.today().year:
         year = int(args[1])
     if 1 <= int(args[2]) <= 25:
         day = int(args[2])
@@ -25,7 +28,7 @@ else:
     print(f"Generated template for day {day} of {year}. Good luck!")
 
 
-part1 = f'''# Part 1
+part1 = f"""# Part 1
 import os
 from aocd import _impartial_submit
 
@@ -42,9 +45,9 @@ workingData = example.readlines()  # uncomment to try example
 
 # uncomment when ready to submit
 # _impartial_submit(answer, day={day}, year={year})
-'''
+"""
 
-part2 = f'''# Part 2
+part2 = f"""# Part 2
 from aocd import _impartial_submit
 
 input = open("./{year}/day_{day}/input.txt")
@@ -57,26 +60,23 @@ workingData = example.readlines()  # uncomment to try example
 
 # uncomment when ready to submit
 # _impartial_submit(answer, day={day}, year={year})
-'''
+"""
 
-neccesaryFiles = ['input.txt', 'example.txt', 'exampleAnswers.txt']
+# Create a dated directory
+dir = Path(str(year)) / f"day_{day}"
+if not dir.exists():
+    dir.mkdir(parents=True, exist_ok=True)
 
-if not os.path.exists(f'{year}'):
-    os.mkdir(f'{year}')
-
-if os.path.exists(f'{year}/day_{day}'):
-    pass
-else:
-    os.mkdir(f'{year}/day_{day}')
-for f in neccesaryFiles:
-    if not os.path.exists(f'{year}/day_{day}/{f}'):
-        with open(f'{year}/day_{day}/{f}', 'w'):
+# Generate input and example files
+for f in ["input.txt", "example.txt", "exampleAnswers.txt"]:
+    if not (dir / f).exists():
+        with open(f"{year}/day_{day}/{f}", "w"):
             pass
-if not os.path.exists(f'{year}/day_{day}/day{day}_1.py'):
-    with open(f'{year}/day_{day}/day{day}_1.py', 'w') as f:
+
+# Generate solution files using the above templates
+if not (dir / f"day{day}_1.py").exists():
+    with open(dir / f"day{day}_1.py", "w") as f:
         f.write(part1)
-    f.close()
-if not os.path.exists(f'{year}/day_{day}/day{day}_2.py'):
-    with open(f'{year}/day_{day}/day{day}_2.py', 'w') as f:
+if not (dir / f"day{day}_2.py").exists():
+    with open(dir / f"day{day}_2.py", "w") as f:
         f.write(part2)
-    f.close()
